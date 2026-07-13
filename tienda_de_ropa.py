9ba246a0e3a4c6e04f0688839fffedb7a240daba
e <fe.ibanezm@duocuc.cl>
2026-07-13 19:30:54 -0400

    Agrego los diccionarios de listas de las prendas y su bodega.

---
+++ tienda_de_ropa.py
@@ -0,0 +1,23 @@
+#  Iniciamos con el diccionario de las prendas.
+# nombre, categoria, talla, color, material, es_unisex
+
+prendas = {
+'S001': ['Polera Basica', 'polera', 'M', 'negro', 'algodon', True],
+'S002': ['Jeans Slim', 'pantalon', 'L', 'azul','denim', False],
+'S003': ['Chaqueta Urban', 'chaqueta', 'M', 'gris', 'poliester', True],
+'S004': ['Vestido Sol', 'vestido', 'S', 'rojo', 'lino', False],
+'S005': ['Poleron Cozy', 'poleron', 'XL', 'verde', 'algodon', True],
+'S006': ['Camisa Formal', 'camisa', 'M', 'blanco', 'algodon', False],
+}
+
+# Diccionario de bodega.
+# precio, unidades.
+
+bodega = {
+'S001': [7990, 12],
+'S002': [19990, 0],
+'S003': [29990, 3],
+'S004': [24990, 6],
+'S005': [17990, 8],
+'S006': [14990, 2]
+}
