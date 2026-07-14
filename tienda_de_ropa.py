a58ce8486e177bbacdbc10c2bffd0c73ede5e227
e <fe.ibanezm@duocuc.cl>
2026-07-13 21:08:09 -0400

    Subir opción uno. (Sé que no está bien del todo, me ganaron los nervios por el tiempo).

--- tienda_de_ropa.py
+++ tienda_de_ropa.py
@@ -6,7 +6,7 @@ prendas = {
 'S003': ['Chaqueta Urban', 'chaqueta', 'M', 'gris', 'poliester', True],
 'S004': ['Vestido Sol', 'vestido', 'S', 'rojo', 'lino', False],
 'S005': ['Poleron Cozy', 'poleron', 'XL', 'verde', 'algodon', True],
-'S006': ['Camisa Formal', 'camisa', 'M', 'blanco', 'algodon', False],
+'S006': ['Camisa Formal', 'camisa', 'M', 'blanco', 'algodon', False]
 }
 
 # Diccionario de bodega.
@@ -19,23 +19,18 @@ bodega = {
 'S005': [17990, 8],
 'S006': [14990, 2]
 }
-
-# Se agrega el menú del programa.
-def menu():
-    print('''
-========== MENÚ PRINCIPAL ==========
-1. Unidades por categoría
-2. Búsqueda de prendas por rango de precio
-3. Actualizar precio de prenda
-4. Agregar prenda
-5. Eliminar prenda
-6. Salir
-=====================================
-''')
     
 # Opción 1:
 def unidades_categoria(categoria):
-    print("")
+    global prendas
+    global bodega
+    acumulador=0
+    for prendas in categoria:
+        for bodega in bodega:
+            acumulador+=1
+        
+    print(f"El total de unidades disponibles es: {acumulador-2}")
+
 
 # Opción 2:
 def busqueda_precio(p_min, p_max):
@@ -55,9 +50,19 @@ def agregar_prenda(codigo, nombre, categoria, talla, color, material, es_unisex,
 def eliminar_prenda(codigo):
     print("")
 
-    
+
 def leer_opcion():
     try:
+        print('''
+========== MENÚ PRINCIPAL ==========
+1. Unidades por categoría
+2. Búsqueda de prendas por rango de precio
+3. Actualizar precio de prenda
+4. Agregar prenda
+5. Eliminar prenda
+6. Salir
+=====================================
+''')
         opcion=int(input("Ingrese opción: "))
         if opcion==1:
             return opcion
@@ -79,10 +84,9 @@ def leer_opcion():
         return None
 
 while True:
-    menu()
     opt=leer_opcion()
     if opt==1:
-        print("Si funciona no se toca")
+        unidades_categoria(categoria=(input("Ingrese categoría a consultar: ").lower()))
     elif opt==2:
         print("")
     elif opt==3:
